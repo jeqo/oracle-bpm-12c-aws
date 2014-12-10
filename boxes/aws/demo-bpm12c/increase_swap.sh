a00b1b4138393029b62b0adc5c2cd5ca22eb361f
Jorge Quilcate <quilcate.jorge@gmail.com>
2014-12-10 16:26:13 -0500

    box updated.

--- boxes/aws/demo-bpm12c/increase_swap.sh
+++ boxes/aws/demo-bpm12c/increase_swap.sh
@@ -4,18 +4,18 @@
 swapsize=$1
 
 # does the swap file already exist?
-grep -q "/var/swap.1" /etc/fstab
+grep -q "swapfile" /etc/fstab
 
 # if not then create it
 if [ $? -ne 0 ]; then
-	echo 'swapfile not found. Adding swapfile.'
-	sudo /bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=${swapsize}
-	chmod 600 /var/swap.1
-	sudo mkswap /var/swap.1
-	sudo swapon /var/swap.1
-	echo '/var/swap.1 swap swap defaults 0 0' >> /etc/fstab
+  echo 'swapfile not found. Adding swapfile.'
+  fallocate -l ${swapsize}M /swapfile
+  chmod 600 /swapfile
+  mkswap /swapfile
+  swapon /swapfile
+  echo '/swapfile none swap defaults 0 0' >> /etc/fstab
 else
-	echo 'swapfile found. No changes made.'
+  echo 'swapfile found. No changes made.'
 fi
 
 # output results to terminal
